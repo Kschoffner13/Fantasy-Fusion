4af7418b8ef7c8217d9981fa67b1f3d3a3a9fc85
Kschoffner13 <koen.schoffner@ucalgary.ca>
2024-02-12 00:11:36 -0700

    added a crude sign in

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Fantasy Fusion</h1>
-                <p>
-                    Ever think one ball wasn't enough. Now is your oppurtunity
-                    to enjoy all the balls you could ever wish for
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Fantasy Fusion</h1>
+        <p>
+          Ever think one ball wasn't enough. Now is your oppurtunity to enjoy
+          all the balls you could ever wish for
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Amplify } from "aws-amplify";
+import config from "./aws-exports";
+import { AmplifyProvider } from "@aws-amplify/ui-react";
+
+Amplify.configure(config);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
