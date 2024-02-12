f8ecf6fc7b6789297f8a5feba78e4ba82a6c1f95
Kschoffner13 <koen.schoffner@ucalgary.ca>
2024-02-12 11:57:49 -0700

    auth refined

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,10 +3,8 @@ export type AmplifyDependentResourcesAttributes = {
     "FantasyFusion": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
