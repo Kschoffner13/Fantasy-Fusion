68f2ea57866f6cbef9dfc853de0f87d9eec53806
Mason <mason.bullock@ucalgary.ca>
2024-02-14 08:45:26 -0700

    add routes and file structure

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "FantasyFusion": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "FantasyFusion": {
       "AppClientID": "string",
---
+++ src/ui-components/FantasyLeagueUpdateForm.d.ts
@@ -0,0 +1,68 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+import * as React from "react";
+import { GridProps, SwitchFieldProps, TextAreaFieldProps, TextFieldProps } from "@aws-amplify/ui-react";
+import { EscapeHatchProps } from "@aws-amplify/ui-react/internal";
+import { FantasyLeague } from "../models";
+export declare type ValidationResponse = {
+    hasError: boolean;
+    errorMessage?: string;
+};
+export declare type ValidationFunction<T> = (value: T, validationResponse: ValidationResponse) => ValidationResponse | Promise<ValidationResponse>;
+export declare type FantasyLeagueUpdateFormInputValues = {
+    Name?: string;
+    OwnerID?: string;
+    Properties?: string;
+    DraftDate?: string;
+    TradeDeadline?: string;
+    PlayoffStartDate?: string;
+    PlayoffTeams?: number;
+    PlayoffMatchupLength?: number;
+    WeeklyPickups?: number;
+    VetoVoteEnabled?: boolean;
+    Schedule?: string;
+};
+export declare type FantasyLeagueUpdateFormValidationValues = {
+    Name?: ValidationFunction<string>;
+    OwnerID?: ValidationFunction<string>;
+    Properties?: ValidationFunction<string>;
+    DraftDate?: ValidationFunction<string>;
+    TradeDeadline?: ValidationFunction<string>;
+    PlayoffStartDate?: ValidationFunction<string>;
+    PlayoffTeams?: ValidationFunction<number>;
+    PlayoffMatchupLength?: ValidationFunction<number>;
+    WeeklyPickups?: ValidationFunction<number>;
+    VetoVoteEnabled?: ValidationFunction<boolean>;
+    Schedule?: ValidationFunction<string>;
+};
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type FantasyLeagueUpdateFormOverridesProps = {
+    FantasyLeagueUpdateFormGrid?: PrimitiveOverrideProps<GridProps>;
+    Name?: PrimitiveOverrideProps<TextFieldProps>;
+    OwnerID?: PrimitiveOverrideProps<TextFieldProps>;
+    Properties?: PrimitiveOverrideProps<TextAreaFieldProps>;
+    DraftDate?: PrimitiveOverrideProps<TextFieldProps>;
+    TradeDeadline?: PrimitiveOverrideProps<TextFieldProps>;
+    PlayoffStartDate?: PrimitiveOverrideProps<TextFieldProps>;
+    PlayoffTeams?: PrimitiveOverrideProps<TextFieldProps>;
+    PlayoffMatchupLength?: PrimitiveOverrideProps<TextFieldProps>;
+    WeeklyPickups?: PrimitiveOverrideProps<TextFieldProps>;
+    VetoVoteEnabled?: PrimitiveOverrideProps<SwitchFieldProps>;
+    Schedule?: PrimitiveOverrideProps<TextAreaFieldProps>;
+} & EscapeHatchProps;
+export declare type FantasyLeagueUpdateFormProps = React.PropsWithChildren<{
+    overrides?: FantasyLeagueUpdateFormOverridesProps | undefined | null;
+} & {
+    id?: string;
+    fantasyLeague?: FantasyLeague;
+    onSubmit?: (fields: FantasyLeagueUpdateFormInputValues) => FantasyLeagueUpdateFormInputValues;
+    onSuccess?: (fields: FantasyLeagueUpdateFormInputValues) => void;
+    onError?: (fields: FantasyLeagueUpdateFormInputValues, errorMessage: string) => void;
+    onChange?: (fields: FantasyLeagueUpdateFormInputValues) => FantasyLeagueUpdateFormInputValues;
+    onValidate?: FantasyLeagueUpdateFormValidationValues;
+} & React.CSSProperties>;
+export default function FantasyLeagueUpdateForm(props: FantasyLeagueUpdateFormProps): React.ReactElement;
