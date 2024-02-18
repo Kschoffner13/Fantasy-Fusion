bb974a4de76748bb3b3122b49bcbc1b2b34ea0e0
Mason <mason.bullock@ucalgary.ca>
2024-02-17 18:34:49 -0700

    draft clock and basic header layout

--- src/Pages/DraftPage.js
+++ src/Pages/DraftPage.js
@@ -1,4 +1,7 @@
+import { NavLink } from "react-router-dom";
 import "../Styles/draftPage.css";
+import logo from "../images/logo.png";
+import { useEffect, useState } from "react";
 
 const DraftPage = () => {
     /*
@@ -16,13 +19,46 @@ const DraftPage = () => {
             - keep track of whose been drafted at which draft position
     */
 
+    const [timeLeft, setTimeLeft] = useState(20);
+
+    useEffect(() => {
+        if (timeLeft > 0) {
+            const timerId = setTimeout(() => {
+                setTimeLeft(timeLeft - 1);
+            }, 1000);
+            return () => clearTimeout(timerId);
+        }
+    }, [timeLeft]);
+
+    const hrs = Math.floor(timeLeft / 3600)
+        .toString()
+        .padStart(2, "0");
+    const mins = Math.floor((timeLeft % 3600) / 60)
+        .toString()
+        .padStart(2, "0");
+    const secs = (timeLeft % 60).toString().padStart(2, "0");
+
     return (
-        <div>
+        <div className="draft-page">
+            <header>
+                <img src={logo} id="logo-img" />
+                <div className="draft-clock">
+                    <h1>{hrs}</h1>
+                    <h1>:</h1>
+                    <h1>{mins}</h1>
+                    <h1>:</h1>
+                    <h1>{secs}</h1>
+                </div>
+                <div className="profile-icon">
+                    <img src="https://picsum.photos/seed/picsum/75/75" />
+                    <div>
+                        <p>Profile Name</p>
+                        <NavLink>Account</NavLink>
+                    </div>
+                </div>
+            </header>
             <div>
-                <h1>Header bar</h1>
-            </div>
-            <div>
-                <h1>Draft Board</h1>
+                <h1 onClick={() => setTimeLeft(3600)}>Draft Board</h1>
             </div>
             <div>
                 <div>Draftable players</div>
