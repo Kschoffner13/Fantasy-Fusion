5f7e3fcb556c1123b044ae4e711dfe01871eb8c5
Mason <mason.bullock@ucalgary.ca>
2024-02-18 02:10:35 -0700

    bed time

--- src/Components/DraftBoard/DraftBoard.js
+++ src/Components/DraftBoard/DraftBoard.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import "./style.css";
 
 const DraftBoard = ({ teams, rounds }) => {
     const picksPerRound = teams.length;
+
+    const [cur, setCur] = useState([1, 2]);
+    const myPickNum = 6;
+
     return (
         <div className="pick-board">
             {Array.from({ length: rounds }, (_, i) => (
@@ -13,10 +18,32 @@ const DraftBoard = ({ teams, rounds }) => {
                               (_, j) => picksPerRound - j
                           )
                     ).map((pick) => (
-                        <div className="pick">
-                            <p key={`${i}-${pick}`}>
-                                {i + 1} - {pick}
-                            </p>
+                        <div
+                            className={`pick ${
+                                i == cur[0] - 1 && pick == cur[1] ? "cur" : ""
+                            } ${
+                                ((i + 1) % 2 == 1 && pick == myPickNum) ||
+                                ((i + 1) % 2 == 0 && 12 - myPickNum + 1 == pick)
+                                    ? "my-pick"
+                                    : ""
+                            }`}
+                        >
+                            {i * 12 + pick >= (cur[0] - 1) * 12 + cur[1] ? (
+                                <div className="not-picked">
+                                    <p key={`${i}-${pick}`}>
+                                        {i + 1} - {pick}
+                                    </p>
+                                </div>
+                            ) : (
+                                <div className="picked">
+                                    <img src="https://cdn.nba.com/headshots/nba/latest/1040x760/1628995.png"></img>
+                                    <div className="pick-info">
+                                        <p>1-1</p>
+                                        <h5>Kevin Knox II, F</h5>
+                                        <p>UFA</p>
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     ))}
                 </div>
