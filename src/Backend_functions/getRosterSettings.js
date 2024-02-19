8596672c19dd365b33ebcf96d04d1ee3144c8bf2
Kschoffner13 <koen.schoffner@ucalgary.ca>
2024-02-18 20:18:10 -0700

    somehow got  datastore working

--- src/Backend_functions/getRosterSettings.js
+++ src/Backend_functions/getRosterSettings.js
@@ -1,8 +1,33 @@
-import { DataStore } from "aws-amplify";
+import { DataStore } from "@aws-amplify/datastore";
 import { FantasyLeague } from "../models";
 
 // pass in the league id
 async function getRosterSettings() {
+  const message = DataStore.save(
+    new FantasyLeague({
+      Name: "Test Name",
+      UserID: "TestUserID",
+      TotalPointsFor: 100,
+      TotalPointsAgainst: 50,
+      MatchUpPoints: 75,
+      Wins: 10,
+      Losses: 5,
+      Draws: 2,
+      Roster: JSON.stringify({
+        player1: "Player Name 1",
+        player2: "Player Name 2",
+      }),
+      PlayerList: JSON.stringify({
+        player1: "Player Name 1",
+        player2: "Player Name 2",
+      }),
+      Lineup: JSON.stringify({
+        player1: "Player Name 1",
+        player2: "Player Name 2",
+      }),
+    })
+  );
+
   const response = await DataStore.query(FantasyLeague);
   console.log(response);
 }
