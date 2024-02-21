912134956ed4a750f51812e81c2d609d560dbc39
Mason <mason.bullock@ucalgary.ca>
2024-02-20 21:46:41 -0700

    basic layout

--- src/Pages/TeamPage.js
+++ src/Pages/TeamPage.js
@@ -1,11 +1,18 @@
 import "../Styles/teamPage.css";
 
 import MainHeader from "../Components/MainHeader/MainHeader.js";
+import RosterSection from "../Components/RosterSection/RosterSection.js";
+import Footer from "../Components/Footer/Footer.js";
 
 const TeamPage = () => {
     return (
         <div className="league-page">
             <MainHeader />
+            <div className="team-info-section">
+                <p>team</p>
+            </div>
+            <RosterSection />
+            <Footer />
         </div>
     );
 };
