00eec497f3b56ad4960a952dc4374761b0bf0b48
Mason <mason.bullock@ucalgary.ca>
2024-02-20 19:48:59 -0700

    page initialization and profile component for header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { withAuthenticator } from "@aws-amplify/ui-react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import DraftPage from "./Pages/DraftPage";
+import LeaguePage from "./Pages/LeaguePage";
 
 function App() {
     return (
@@ -11,6 +12,10 @@ function App() {
                 <Routes>
                     <Route exact path="/" element={<Home />} />
                     <Route path="/:leagueName/draft" element={<DraftPage />} />
+                    <Route
+                        path="/:leagueName/:teamName"
+                        element={<LeaguePage />}
+                    />
                 </Routes>
             </HashRouter>
             {/*  */}
---
+++ src/Components/ProfileHeaderSection/ProfileHeaderSection.js
@@ -0,0 +1,34 @@
+import "./style.css";
+import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchUserAttributes } from "@aws-amplify/auth";
+
+const ProfileHeaderSection = () => {
+    const [username, setUsername] = useState("NAME");
+
+    const getUserName = async () => {
+        try {
+            const username = (await fetchUserAttributes()).preferred_username;
+            setUsername(username);
+            return <p>username</p>;
+        } catch (error) {
+            console.log("error getting user", error);
+        }
+    };
+
+    useEffect(() => {
+        getUserName();
+    }, []);
+
+    return (
+        <div className="profile-icon">
+            <img src="https://picsum.photos/seed/picsum/75/75" />
+            <div className="profile-icon-info">
+                <p>{username}</p>
+                <NavLink>Account</NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default ProfileHeaderSection;
