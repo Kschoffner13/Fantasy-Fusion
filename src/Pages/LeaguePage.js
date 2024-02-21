f42e774db37e1f07fe05c3e398b114d7519d5e3c
Mason <mason.bullock@ucalgary.ca>
2024-02-20 21:09:23 -0700

    header

--- src/Pages/LeaguePage.js
+++ src/Pages/LeaguePage.js
@@ -1,19 +1,42 @@
+import { Route, Routes } from "react-router";
 import ProfileHeaderSection from "../Components/ProfileHeaderSection/ProfileHeaderSection";
 import "../Styles/leaguePage.css";
 import logo from "../images/logo.png";
+import Home from "../Pages/Home.js";
+import { HashRouter, NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const LeaguePage = () => {
+    const teamName = "myTeam";
+    const leagueName = "myLeague";
+
     return (
         <div className="league-page">
             <header>
                 <div className="header-top">
-                    <img src={logo}></img>
-                    <h6>Home</h6>
-                    <h6>My Teams</h6>
-                    <h6>How to Play</h6>
+                    <img className="header-logo" src={logo}></img>
+                    <NavLink to="/">
+                        <h5>Home</h5>
+                    </NavLink>
+                    <h5>My Teams</h5> {/*MAKE THIS A DROP DOWN*/}
+                    <NavLink to="/help">
+                        <h5>How to Play</h5>
+                    </NavLink>
                     <ProfileHeaderSection />
                 </div>
-                <div className="header-bot"></div>
+                <div className="header-bot">
+                    <h5>League Name</h5>
+                    <NavLink to={`/${leagueName}/${teamName}`}>My Team</NavLink>
+                    <NavLink to={`/${leagueName}/${teamName}/matchup`}>
+                        Matchup
+                    </NavLink>
+                    <NavLink to={`/${leagueName}/${teamName}/players`}>
+                        Free Agents
+                    </NavLink>
+                    <NavLink to={`/${leagueName}`}>League</NavLink>
+
+                    <h6>THIS COULD BE YOUR COMPANYS AD!</h6>
+                </div>
             </header>
         </div>
     );
