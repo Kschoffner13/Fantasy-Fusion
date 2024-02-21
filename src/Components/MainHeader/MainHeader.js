ac2cdc6e1337d8cd2e061e6a75384c28765359a2
Mason <mason.bullock@ucalgary.ca>
2024-02-20 21:21:22 -0700

    main header component

---
+++ src/Components/MainHeader/MainHeader.js
@@ -0,0 +1,40 @@
+import "./style.css";
+import logo from "../../images/logo.png";
+import { NavLink } from "react-router-dom";
+import ProfileHeaderSection from "../ProfileHeaderSection/ProfileHeaderSection";
+
+const MainHeader = () => {
+    const teamName = "myTeam";
+    const leagueName = "myLeague";
+
+    return (
+        <header>
+            <div className="header-top">
+                <img className="header-logo" src={logo}></img>
+                <NavLink to="/">
+                    <h5>Home</h5>
+                </NavLink>
+                <h5>My Teams</h5> {/*MAKE THIS A DROP DOWN*/}
+                <NavLink to="/help">
+                    <h5>How to Play</h5>
+                </NavLink>
+                <ProfileHeaderSection />
+            </div>
+            <div className="header-bot">
+                <h5>League Name</h5>
+                <NavLink to={`/${leagueName}/${teamName}`}>My Team</NavLink>
+                <NavLink to={`/${leagueName}/${teamName}/matchup`}>
+                    Matchup
+                </NavLink>
+                <NavLink to={`/${leagueName}/${teamName}/players`}>
+                    Free Agents
+                </NavLink>
+                <NavLink to={`/${leagueName}`}>League</NavLink>
+
+                <h6>THIS COULD BE YOUR COMPANYS AD!</h6>
+            </div>
+        </header>
+    );
+};
+
+export default MainHeader;
--- src/Pages/TeamPage.js
+++ src/Pages/TeamPage.js
@@ -1,43 +1,11 @@
-import { Route, Routes } from "react-router";
-import ProfileHeaderSection from "../Components/ProfileHeaderSection/ProfileHeaderSection.js";
 import "../Styles/teamPage.css";
-import logo from "../images/logo.png";
-import Home from "./Home.js";
-import { HashRouter, NavLink } from "react-router-dom";
-import { useState } from "react";
+
+import MainHeader from "../Components/MainHeader/MainHeader.js";
 
 const TeamPage = () => {
-    const teamName = "myTeam";
-    const leagueName = "myLeague";
-
     return (
         <div className="league-page">
-            <header>
-                <div className="header-top">
-                    <img className="header-logo" src={logo}></img>
-                    <NavLink to="/">
-                        <h5>Home</h5>
-                    </NavLink>
-                    <h5>My Teams</h5> {/*MAKE THIS A DROP DOWN*/}
-                    <NavLink to="/help">
-                        <h5>How to Play</h5>
-                    </NavLink>
-                    <ProfileHeaderSection />
-                </div>
-                <div className="header-bot">
-                    <h5>League Name</h5>
-                    <NavLink to={`/${leagueName}/${teamName}`}>My Team</NavLink>
-                    <NavLink to={`/${leagueName}/${teamName}/matchup`}>
-                        Matchup
-                    </NavLink>
-                    <NavLink to={`/${leagueName}/${teamName}/players`}>
-                        Free Agents
-                    </NavLink>
-                    <NavLink to={`/${leagueName}`}>League</NavLink>
-
-                    <h6>THIS COULD BE YOUR COMPANYS AD!</h6>
-                </div>
-            </header>
+            <MainHeader />
         </div>
     );
 };
