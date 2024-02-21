f26a763fe9406416cb97dc481954aba2481986bc
Kschoffner13 <koen.schoffner@ucalgary.ca>
2024-02-21 10:53:08 -0700

    added a make pic function

--- src/Backend_functions/DraftAccessor.js
+++ src/Backend_functions/DraftAccessor.js
@@ -55,6 +55,47 @@ class DraftAccessor {
     );
     return response[0]["DraftDate"];
   }
+
+  // this function takes in a player id and the irl league and
+  // will update the draft model, and will increment the current pick number by 1
+  // as well as add the pick time to the pick deadline and save it to the drafty model
+
+  async makePick(playerID, irlLeague) {
+    // retrive the draft object
+    const original = await DataStore.query(Draft, (c) =>
+      c.fantasyleagueID.eq(this.FLID)
+    );
+    let newdata = null;
+
+    // create the new player drafted object that gets appened to the playersDrafted list
+    const newPlayerDrafted = {
+      pick: original[0].curentPick + 1,
+      playerID: playerID,
+      league: irlLeague,
+    };
+
+    const oldPlayersDrafted = original[0].playersDrafted["picks"];
+
+    if (oldPlayersDrafted.length === 0) {
+      newdata = {
+        picks: [newPlayerDrafted],
+      };
+    } else {
+      newdata = oldPlayersDrafted.concat(newPlayerDrafted);
+    }
+
+    console.log(newdata);
+
+    const response = await DataStore.save(
+      Draft.copyOf(original[0], (updated) => {
+        updated["curentPick"] = updated["curentPick"] + 1;
+        updated["playersDrafted"] = JSON.stringify(newdata);
+      })
+    );
+  }
 }
 
+// notes for of where i left off: need to be ablke to
+//append data to end the of the picks sectrion in the awsjson object
+// also need to accout for when we need to increment the pickdealine
 export default DraftAccessor;
