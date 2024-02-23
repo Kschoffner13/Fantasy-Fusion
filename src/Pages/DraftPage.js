b769c86f4adc52910640816aabbb6d1c37640974
Kschoffner13 <koen.schoffner@ucalgary.ca>
2024-02-22 17:17:32 -0700

    got realtime bs working

--- src/Pages/DraftPage.js
+++ src/Pages/DraftPage.js
@@ -6,9 +6,11 @@ import DraftBoard from "../Components/DraftBoard/DraftBoard";
 import { fetchUserAttributes } from "@aws-amplify/auth";
 import TeamPane from "../Components/TeamPane/TeamPane";
 import DraftPane from "../Components/DraftPane/DraftPane";
+import { Draft } from "../models";
+import { DataStore } from "@aws-amplify/datastore";
 
 const DraftPage = () => {
-    /*
+  /*
         BACKEND INFORMATION:
         Grab roster/draft setttings from league
         Grab drafted players (Player name, Position, team drafted, what round they were drafted)
@@ -22,204 +24,208 @@ const DraftPage = () => {
             - keep track of the active pick and timeleft to pick
             - keep track of whose been drafted at which draft position
     */
-    const rounds = 12;
-    const [username, setUsername] = useState("NAME");
 
-    const [teams, setTeams] = useState([
-        {
-            name: "The Alchemists",
-            logoUrl: "https://picsum.photos/id/12/200/200",
-        },
-        { name: "The Wizards", logoUrl: "https://picsum.photos/id/13/200/200" },
-        {
-            name: "The Highlanders",
-            logoUrl: "https://picsum.photos/id/14/200/200",
-        },
-        {
-            name: "The Pioneers",
-            logoUrl: "https://picsum.photos/id/15/200/200",
-        },
-        {
-            name: "The Gladiators",
-            logoUrl: "https://picsum.photos/id/16/200/200",
-        },
-        {
-            name: "The Conquerors",
-            logoUrl: "https://picsum.photos/id/17/200/200",
-        },
-        { name: "The Titans", logoUrl: "https://picsum.photos/id/18/200/200" },
-        { name: "The Dragons", logoUrl: "https://picsum.photos/id/19/200/200" },
-        {
-            name: "The Challengers",
-            logoUrl: "https://picsum.photos/id/20/200/200",
-        },
-        {
-            name: "The Avengers",
-            logoUrl: "https://picsum.photos/id/21/200/200",
-        },
-        {
-            name: "The Guardians",
-            logoUrl: "https://picsum.photos/id/22/200/200",
-        },
-        {
-            name: "The Warriors",
-            logoUrl: "https://picsum.photos/id/23/200/200",
-        },
-    ]);
-
-    const [rosterFormat, setRosterFormat] = useState({
-        GRD: 2,
-        FWD: 2,
-        CB: 1,
-        CH: 1,
-        WNG: 2,
-        DF: 2,
-        GL: 1,
-        UTL: 3,
-        BCH: 8,
+  function sub() {
+    const id = "03b7a95b-3a99-41c5-8355-dbe40acfa051";
+    const subscription = DataStore.observe(Draft, id).subscribe((msg) => {
+      console.log(msg);
     });
+  }
 
-    const [availablePlayers, setAvailablePlayers] = useState([]);
+  let intervalId = setInterval(sub, 3000);
 
-    const [timeLeft, setTimeLeft] = useState(120);
+  const rounds = 12;
+  const [username, setUsername] = useState("NAME");
 
-    const getUserName = async () => {
-        try {
-            const username = (await fetchUserAttributes()).preferred_username;
-            setUsername(username);
-            return <p>username</p>;
-        } catch (error) {
-            console.log("error getting user", error);
-        }
+  const [teams, setTeams] = useState([
+    {
+      name: "The Alchemists",
+      logoUrl: "https://picsum.photos/id/12/200/200",
+    },
+    { name: "The Wizards", logoUrl: "https://picsum.photos/id/13/200/200" },
+    {
+      name: "The Highlanders",
+      logoUrl: "https://picsum.photos/id/14/200/200",
+    },
+    {
+      name: "The Pioneers",
+      logoUrl: "https://picsum.photos/id/15/200/200",
+    },
+    {
+      name: "The Gladiators",
+      logoUrl: "https://picsum.photos/id/16/200/200",
+    },
+    {
+      name: "The Conquerors",
+      logoUrl: "https://picsum.photos/id/17/200/200",
+    },
+    { name: "The Titans", logoUrl: "https://picsum.photos/id/18/200/200" },
+    { name: "The Dragons", logoUrl: "https://picsum.photos/id/19/200/200" },
+    {
+      name: "The Challengers",
+      logoUrl: "https://picsum.photos/id/20/200/200",
+    },
+    {
+      name: "The Avengers",
+      logoUrl: "https://picsum.photos/id/21/200/200",
+    },
+    {
+      name: "The Guardians",
+      logoUrl: "https://picsum.photos/id/22/200/200",
+    },
+    {
+      name: "The Warriors",
+      logoUrl: "https://picsum.photos/id/23/200/200",
+    },
+  ]);
+
+  const [rosterFormat, setRosterFormat] = useState({
+    GRD: 2,
+    FWD: 2,
+    CB: 1,
+    CH: 1,
+    WNG: 2,
+    DF: 2,
+    GL: 1,
+    UTL: 3,
+    BCH: 8,
+  });
+
+  const [availablePlayers, setAvailablePlayers] = useState([]);
+
+  const [timeLeft, setTimeLeft] = useState(120);
+
+  const getUserName = async () => {
+    try {
+      const username = (await fetchUserAttributes()).preferred_username;
+      setUsername(username);
+      return <p>username</p>;
+    } catch (error) {
+      console.log("error getting user", error);
+    }
+  };
+
+  const getPlayers = async () => {
+    try {
+      const response = await fetch(
+        "https://c77xo6r7ghtdvd57xdm5megfay0qfvsc.lambda-url.ca-central-1.on.aws/"
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setAvailablePlayers(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    getUserName();
+    getPlayers();
+  }, []);
+
+  useEffect(() => {
+    if (timeLeft > 0) {
+      const timerId = setTimeout(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
+    }
+  }, [timeLeft]);
+
+  const hrs = Math.floor(timeLeft / 3600)
+    .toString()
+    .padStart(2, "0");
+  const mins = Math.floor((timeLeft % 3600) / 60)
+    .toString()
+    .padStart(2, "0");
+  const secs = (timeLeft % 60).toString().padStart(2, "0");
+
+  const [height, setHeight] = useState(200); // Initial height of the resizable div
+  let pageHeight = document.documentElement.scrollHeight;
+  let headerheight = document.querySelector(".draft-header")?.offsetHeight; // replace '.draft-board' with the selector for your element
+
+  const handleMouseDown = (e) => {
+    const startY = e.clientY;
+
+    const handleMouseMove = (e) => {
+      const newHeight = height - (e.clientY - startY);
+      setHeight(newHeight);
     };
 
-    const getPlayers = async () => {
-        try {
-            const response = await fetch(
-                "https://c77xo6r7ghtdvd57xdm5megfay0qfvsc.lambda-url.ca-central-1.on.aws/"
-            );
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            const data = await response.json();
-            console.log(data);
-            setAvailablePlayers(data);
-        } catch (error) {
-            console.error("Error:", error);
-        }
+    const handleMouseUp = () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
     };
 
-    useEffect(() => {
-        getUserName();
-        getPlayers();
-    }, []);
-
-    useEffect(() => {
-        if (timeLeft > 0) {
-            const timerId = setTimeout(() => {
-                setTimeLeft(timeLeft - 1);
-            }, 1000);
-            return () => clearTimeout(timerId);
-        }
-    }, [timeLeft]);
-
-    const hrs = Math.floor(timeLeft / 3600)
-        .toString()
-        .padStart(2, "0");
-    const mins = Math.floor((timeLeft % 3600) / 60)
-        .toString()
-        .padStart(2, "0");
-    const secs = (timeLeft % 60).toString().padStart(2, "0");
-
-    const [height, setHeight] = useState(200); // Initial height of the resizable div
-    let pageHeight = document.documentElement.scrollHeight;
-    let headerheight = document.querySelector(".draft-header")?.offsetHeight; // replace '.draft-board' with the selector for your element
-
-    const handleMouseDown = (e) => {
-        const startY = e.clientY;
-
-        const handleMouseMove = (e) => {
-            const newHeight = height - (e.clientY - startY);
-            setHeight(newHeight);
-        };
-
-        const handleMouseUp = () => {
-            window.removeEventListener("mousemove", handleMouseMove);
-            window.removeEventListener("mouseup", handleMouseUp);
-        };
-
-        window.addEventListener("mousemove", handleMouseMove);
-        window.addEventListener("mouseup", handleMouseUp);
-    };
-    return (
-        <div className="draft-page">
-            <header className="draft-header">
-                <img src={logo} id="logo-img" />
-                <div className="name-heading">
-                    <h2>Leauge Name</h2>
-                    <h3>Team Name</h3>
-                </div>
-                <div
-                    className="draft-clock"
-                    id={timeLeft < 15 ? "time-alert" : ""}
-                >
-                    <div className="time-section">
-                        <h1>{hrs}</h1>
-                        <p>Hours</p>
-                    </div>
-                    <h1>:</h1>
-                    <div className="time-section">
-                        <h1>{mins}</h1>
-                        <p>Minutes</p>
-                    </div>
-                    <h1>:</h1>
-                    <div className="time-section">
-                        <h1>{secs}</h1>
-                        <p>Seconds</p>
-                    </div>
-                </div>
-                <div className="pick-info">
-                    <h4>You're Up in 5 picks!</h4>
-                    <p>Current Pick: 1-1</p>
-                </div>
-                <div className="profile-icon">
-                    <img src="https://picsum.photos/seed/picsum/75/75" />
-                    <div className="profile-icon-info">
-                        <p>{username}</p>
-                        <NavLink>Account</NavLink>
-                    </div>
-                </div>
-            </header>
-            <div
-                className="draft-board"
-                style={{ height: `${pageHeight - height - headerheight}px` }}
-            >
-                <div className="board-head">
-                    {teams.map((team, index) => (
-                        <div key={index} className="team-block">
-                            <img src={team.logoUrl} alt={team.name} />
-                            <h2>{team.name}</h2>
-                        </div>
-                    ))}
-                </div>
-                <DraftBoard rounds={rounds} teams={teams} />
-            </div>
-
-            <div
-                className="user-section resizeable"
-                style={{ height: `${height}px` }}
-            >
-                <div
-                    className="resizeable-handle"
-                    onMouseDown={handleMouseDown}
-                ></div>
-                <DraftPane players={availablePlayers} />
-                <TeamPane format={rosterFormat} teams={teams} />
-            </div>
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseup", handleMouseUp);
+  };
+  return (
+    <div className="draft-page">
+      <header className="draft-header">
+        <img src={logo} id="logo-img" />
+        <div className="name-heading">
+          <h2>Leauge Name</h2>
+          <h3>Team Name</h3>
         </div>
-    );
+        <div className="draft-clock" id={timeLeft < 15 ? "time-alert" : ""}>
+          <div className="time-section">
+            <h1>{hrs}</h1>
+            <p>Hours</p>
+          </div>
+          <h1>:</h1>
+          <div className="time-section">
+            <h1>{mins}</h1>
+            <p>Minutes</p>
+          </div>
+          <h1>:</h1>
+          <div className="time-section">
+            <h1>{secs}</h1>
+            <p>Seconds</p>
+          </div>
+        </div>
+        <div className="pick-info">
+          <h4>You're Up in 5 picks!</h4>
+          <p>Current Pick: 1-1</p>
+        </div>
+        <div className="profile-icon">
+          <img src="https://picsum.photos/seed/picsum/75/75" />
+          <div className="profile-icon-info">
+            <p>{username}</p>
+            <NavLink>Account</NavLink>
+          </div>
+        </div>
+      </header>
+      <div
+        className="draft-board"
+        style={{ height: `${pageHeight - height - headerheight}px` }}
+      >
+        <div className="board-head">
+          {teams.map((team, index) => (
+            <div key={index} className="team-block">
+              <img src={team.logoUrl} alt={team.name} />
+              <h2>{team.name}</h2>
+            </div>
+          ))}
+        </div>
+        <DraftBoard rounds={rounds} teams={teams} />
+      </div>
+
+      <div
+        className="user-section resizeable"
+        style={{ height: `${height}px` }}
+      >
+        <div className="resizeable-handle" onMouseDown={handleMouseDown}></div>
+        <DraftPane players={availablePlayers} />
+        <TeamPane format={rosterFormat} teams={teams} />
+      </div>
+    </div>
+  );
 };
 
 export default DraftPage;
