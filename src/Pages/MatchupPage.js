ef459eeb5e196daf89a6d4166f6c27f9a648e456
Mason <mason.bullock@ucalgary.ca>
2024-03-11 13:30:21 -0600

    file init

---
+++ src/Pages/MatchupPage.js
@@ -0,0 +1,10 @@
+const MatchupPage = () => {
+    return (
+        <div>
+            <div className="user-team-section"></div>
+            <div className="opponent-team-section"></div>
+        </div>
+    );
+};
+
+export default MatchupPage;
