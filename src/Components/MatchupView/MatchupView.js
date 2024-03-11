bf36865efca6ac74c3eee841c55b5cd7d9d38da5
Mason <mason.bullock@ucalgary.ca>
2024-03-11 15:58:58 -0600

    page layout basic

--- src/Components/MatchupView/MatchupView.js
+++ src/Components/MatchupView/MatchupView.js
@@ -1,11 +1,16 @@
 import "./style.css";
 import logo from "../../images/logo.png";
+import Table from "../Table/Table.js";
+import { useState } from "react";
 
 const MatchupView = ({ team }) => {
     /*
         Team name/logo - points
         starters w/ games table
     */
+
+    const [format, setFormat] = useState(team.format);
+
     return (
         <div>
             <div>
@@ -15,6 +20,33 @@ const MatchupView = ({ team }) => {
                 </div>
                 <h1>123.45</h1>
             </div>
+            <Table
+                headers={["name", "FP"]}
+                roster={team.roster}
+                rosterPlacement={team.format}
+                setRosterPlacement={setFormat}
+                filterKeys={["GRD", "FWD", "CB", "SM"]}
+                glStats={null}
+                hasButton={false}
+            />
+            <Table
+                headers={["name", "FP"]}
+                roster={team.roster}
+                rosterPlacement={team.format}
+                setRosterPlacement={setFormat}
+                filterKeys={["CH", "WNG", "DEF", "GL"]}
+                glStats={["name", "FP"]}
+                hasButton={false}
+            />
+            <Table
+                headers={["name", "FP"]}
+                roster={team.roster}
+                rosterPlacement={team.format}
+                filterKeys={["UTL"]}
+                setRosterPlacement={setFormat}
+                glStats={null}
+                hasButton={false}
+            />
         </div>
     );
 };
