abf983e9afcf3566e10449cbb0b71bc4efea2d5d
Mason <mason.bullock@ucalgary.ca>
2024-03-11 15:33:21 -0600

    commit message

--- src/Pages/MatchupPage.js
+++ src/Pages/MatchupPage.js
@@ -1,7 +1,11 @@
+import MatchupView from "../Components/MatchupView/MatchupView";
+
 const MatchupPage = () => {
     return (
         <div>
-            <div className="user-team-section"></div>
+            <div className="user-team-section">
+                <MatchupView team={{ name: "test" }} />
+            </div>
             <div className="opponent-team-section"></div>
         </div>
     );
