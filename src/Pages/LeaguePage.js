a1df19fcd4ef0e3c8c05fe572bda2e75c0306e93
Mason <mason.bullock@ucalgary.ca>
2024-03-15 17:14:55 -0600

    pre rebase

--- src/Pages/LeaguePage.js
+++ src/Pages/LeaguePage.js
@@ -1,10 +1,22 @@
+import { useParams } from "react-router";
 import MainHeader from "../Components/MainHeader/MainHeader";
 import "../Styles/leaguePage.css";
 
 const LeaguePage = () => {
+
+    //fp leaders
+    //standings
+    //matchup snapshots
+    //transactions
+
+    const { leagueName } = useParams();
+
+    console.log(leagueName);
+
     return (
         <div className="league-page">
             <MainHeader />
+            <h1>{leagueName}</h1>
         </div>
     );
 };
