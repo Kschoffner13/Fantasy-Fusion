066528e5695149c91852d59d1dde20fe26188bc4
Mason <mason.bullock@ucalgary.ca>
2024-03-14 18:50:25 -0600

    basic layout

--- src/Pages/FreeAgentsPage.js
+++ src/Pages/FreeAgentsPage.js
@@ -1,7 +1,22 @@
+import MainHeader from "../Components/MainHeader/MainHeader";
 import "../Styles/freeAgentsPage.css";
+import Table from "../Components/Table/Table.js";
 
 const FreeAgentsPage = () => {
-    return <h1>FA PAGE</h1>;
+    return (
+        <div className="fa-page">
+            <MainHeader />
+            <div>FILTERS</div>
+            <Table
+                headers={["test"]}
+                roster={["test"]}
+                rosterPlacement={[]}
+                setRosterPlacement={null}
+                filterKeys={null}
+                title={"players"}
+            />
+        </div>
+    );
 };
 
 export default FreeAgentsPage;
