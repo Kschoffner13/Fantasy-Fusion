89fb47bf59348a0221ad1c1f699000b9e87e5843
Mason <mason.bullock@ucalgary.ca>
2024-03-15 17:59:00 -0600

    frontend layout

--- src/Pages/LeaguePage.js
+++ src/Pages/LeaguePage.js
@@ -1,6 +1,8 @@
 import { useParams } from "react-router";
 import MainHeader from "../Components/MainHeader/MainHeader";
 import "../Styles/leaguePage.css";
+import Table from "../Components/Table/Table";
+import MatchupSnapshot from "../Components/MatchupSnapshot/MatchupSnapshot";
 
 const LeaguePage = () => {
     //fp leaders
@@ -15,7 +17,42 @@ const LeaguePage = () => {
     return (
         <div className="league-page">
             <MainHeader />
+            <div className="matchup-snapshots">
+                <MatchupSnapshot />
+                <MatchupSnapshot />
+                <MatchupSnapshot />
+                <MatchupSnapshot />
+                <MatchupSnapshot />
+                <MatchupSnapshot />
+            </div>
             <h1>{leagueName}</h1>
+            <div className="league-tables">
+                <Table
+                    headers={["test"]}
+                    roster={["test"]}
+                    rosterPlacement={[]}
+                    setRosterPlacement={null}
+                    filterKeys={null}
+                    title={"Fantasy Leaders"}
+                />
+
+                <Table
+                    headers={["test"]}
+                    roster={["test"]}
+                    rosterPlacement={[]}
+                    setRosterPlacement={null}
+                    filterKeys={null}
+                    title={"Standings"}
+                />
+                <Table
+                    headers={["test"]}
+                    roster={["test"]}
+                    rosterPlacement={[]}
+                    setRosterPlacement={null}
+                    filterKeys={null}
+                    title={"Transactions"}
+                />
+            </div>
         </div>
     );
 };
