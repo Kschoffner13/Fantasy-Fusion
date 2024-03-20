7bc6ece2afdcdb145f81e83d3a0c94ae970abdf2
Kschoffner13 <koen.schoffner@ucalgary.ca>
2024-03-20 11:44:05 -0600

    changed Accessor classes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,9 @@
 import { NavLink } from "react-router-dom";
 import logo from "../images/logo.png";
 import { signOut } from "aws-amplify/auth";
-import { Button } from "@aws-amplify/ui-react";
-
-import TeamAccessor from "../Accessors/TeamAccessor";
+import { Button } from "@aws-amplify/ui-react"
+import FLAccessor from "../Accessors/FLAccessor";
+import inviteClass from "../Accessors/InviteClass";
 
 async function handleSignOut() {
   try {
@@ -15,11 +15,34 @@ async function handleSignOut() {
 
 
 async function test() {
-  const teamAccessor = new TeamAccessor("123456", "123456");
+  const FLA = new FLAccessor("654321");
 
-  const response = await teamAccessor.getUsersTeams();
+  const newL = {
+    Name: "Best League Ever",
+
+    Properties: { property1: "value1", property2: "value2" },
+    DraftDate: Date.now(),
+    TradeDeadline: Date.now(),
+    PlayoffStartDate: Date.now(),
+    PlayoffTeams: 8,
+    PlayoffMatchupLength: 2,
+    WeeklyPickups: 5,
+    VetoVoteEnabled: true,
+
+    Schedule: { week1: "Team1 vs Team2", week2: "Team3 vs Team4" },
+  };
+  const response1 = await FLA.saveFantasyLeague(newL.Name, newL.Properties, newL.DraftDate, newL.TradeDeadline, newL.PlayoffStartDate,
+    newL.PlayoffTeams, newL.PlayoffMatchupLength, newL.WeeklyPickups, newL.VetoVoteEnabled, newL.Schedule);
+
+  const response2 = await FLA.updateFantasyLeague("1cb1b891-0b06-48e4-a843-3ed8ed0b7ecf",
+    { Name: "Second best", VetoVoteEnabled: false, Schedule: { week1: "Team1 vs Team2", week2: "Team3 vs Team4" } });
+  console.log(response1);
+}
+
+async function test2() {
+  const I = new inviteClass();
+  const response = await I.sendInvite("kschoffner@gmail.com");
   console.log(response);
-
 }
 
 const Home = () => {
@@ -43,7 +66,7 @@ const Home = () => {
       <NavLink to="/abc/draft">DRAFT</NavLink>
       <NavLink to="/abc/def">TEAM</NavLink>
       <NavLink to="/abc/matchup">MATCHUP</NavLink>
-      <Button onClick={test}>test</Button>
+      <Button onClick={test2}>test</Button>
 
     </header>
   );
