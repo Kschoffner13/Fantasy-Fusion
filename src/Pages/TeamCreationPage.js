ac59554d569923a0695cf2b0f21b192c3a529b1f
Mason <mason.bullock@ucalgary.ca>
2024-03-20 18:51:56 -0600

    team creation framework

--- src/Pages/TeamCreationPage.js
+++ src/Pages/TeamCreationPage.js
@@ -2,11 +2,15 @@ import "../Styles/teamCreationPage.css";
 import TeamAccessor from "../Accessors/TeamAccessor";
 import { useNavigate, useParams } from "react-router";
 import { useEffect } from "react";
+import { useState } from "react";
+import { getCurrentUser } from "@aws-amplify/auth";
 
 const TeamCreationPage = () => {
     const { leagueName } = useParams();
     const nav = useNavigate();
-    const teamAccessor = new TeamAccessor();
+    const [teamAccessor, setTeamAccessor] = useState(null);
+
+    const [teamName, setTeamName] = useState("");
 
     console.log(leagueName);
 
@@ -17,15 +21,79 @@ const TeamCreationPage = () => {
                 nav("/");
             }
         } catch {
-            console("INVALID LEAGUE");
+            console.log("INVALID LEAGUE");
+        }
+    };
+
+    const initAccessor = async () => {
+        try {
+            const user = await getCurrentUser();
+            console.log(user);
+            setTeamAccessor(new TeamAccessor(leagueName, user.userId));
+        } catch {
+            console.log("FAILED TO GET USER");
         }
     };
 
     useEffect(() => {
+        initAccessor();
         verifyLeagueCode();
     }, []);
 
-    return <h1>Create Team</h1>;
+    const handleButtonClick = async () => {
+        console.log(`Button clicked with team name: ${teamName}`);
+        const rosterPlacement = {
+            //Change this to grab from league settings in the future
+            GRD1: null,
+            GRD2: null,
+            FWD1: null,
+            FWD2: null,
+            CB: null,
+            SM: null,
+            CH: null,
+            WNG1: null,
+            WNG2: null,
+            DEF1: null,
+            DEF2: null,
+            GL: null,
+            UTL1: null,
+            UTL2: null,
+            UTL3: null,
+            BCH1: null,
+            BCH2: null,
+            BCH3: null,
+            BCH4: null,
+            BCH5: null,
+            BCH6: null,
+            BCH7: null,
+            BCH8: null,
+        };
+        await teamAccessor.saveTeam(
+            teamName,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            null,
+            null,
+            rosterPlacement
+        );
+    };
+
+    return (
+        <div>
+            <h1>Create Team</h1>
+            <input
+                type="text"
+                value={teamName}
+                onChange={(e) => setTeamName(e.target.value)}
+                placeholder="Enter team name"
+            />
+            <button onClick={handleButtonClick}>Submit</button>
+        </div>
+    );
 };
 
 export default TeamCreationPage;
