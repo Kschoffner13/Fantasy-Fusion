53415dc4aa3558e1c1d710af15b6a061b2ffabfb
Kschoffner13 <koen.schoffner@ucalgary.ca>
2024-03-22 15:08:33 -0600

    small update

--- src/App.js
+++ src/App.js
@@ -7,51 +7,31 @@ import TeamPage from "./Pages/TeamPage";
 import MatchupPage from "./Pages/MatchupPage";
 import FreeAgentsPage from "./Pages/FreeAgentsPage";
 import LeaguePage from "./Pages/LeaguePage";
-import HowToPlayPage from "./Pages/HowToPlayPage";
-import NBAStatsPage from "./Pages/NBAStatsPage";
-import LandingHomePage from "./Pages/LandingHomePage";
-import NHLStatsPage from "./Pages/NHLStatsPage";
 import TeamCreationPage from "./Pages/TeamCreationPage";
 
 import LeagueCreationPage from "./Pages/LeagueCreationPage.js";
 
 function App() {
-    return (
-        <div className="App">
-            <HashRouter>
-                <Routes>
-                    <Route exact path="/" element={<LandingHomePage />} />
-                    <Route path="/nbastats" element={<NBAStatsPage />} />
-                    <Route path="/howtoplay" element={<HowToPlayPage />} />
-                    <Route path="/nhlstats" element={<NHLStatsPage />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/:leagueName/draft" element={<DraftPage />} />
-                    <Route
-                        path="/:leagueName/:teamName"
-                        element={<TeamPage />}
-                    />
-                    <Route
-                        path="/:leagueName/matchup"
-                        element={<MatchupPage />}
-                    />
-                    <Route
-                        path="/:leagueName/players"
-                        element={<FreeAgentsPage />}
-                    />
-                    <Route path="/:leagueName" element={<LeaguePage />} />
-                    <Route
-                        path="/:leagueName/createteam"
-                        element={<TeamCreationPage />}
-                    />
-                    <Route
-                        path="/createleague"
-                        element={<LeagueCreationPage />}
-                    />
-                </Routes>
-            </HashRouter>
-            {/*  */}
-        </div>
-    );
+  return (
+    <div className="App">
+      <HashRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/:leagueName/draft" element={<DraftPage />} />
+          <Route path="/:leagueName/:teamName" element={<TeamPage />} />
+          <Route path="/:leagueName/matchup" element={<MatchupPage />} />
+          <Route path="/:leagueName/players" element={<FreeAgentsPage />} />
+          <Route path="/:leagueName" element={<LeaguePage />} />
+          <Route
+            path="/:leagueName/createteam"
+            element={<TeamCreationPage />}
+          />
+          <Route path="/createleague" element={<LeagueCreationPage />} />
+        </Routes>
+      </HashRouter>
+      {/*  */}
+    </div>
+  );
 }
 
 export default withAuthenticator(App);
