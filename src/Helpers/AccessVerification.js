432d01f45b17da69d7ed6664175c7acecbbbc298
Mason <mason.bullock@ucalgary.ca>
2024-03-22 05:38:56 -0600

    verifyTeamAccess function functional and tested (Positive for gonorrhea)

--- src/Helpers/AccessVerification.js
+++ src/Helpers/AccessVerification.js
@@ -1,13 +1,27 @@
+import TeamAccessor from "../Accessors/TeamAccessor";
+
 class AccessVerification {
-    constructor(userId) {
+    constructor(userId, leagueId) {
         this.userId = userId;
+        this.leagueId = leagueId;
     }
 
-    verifyTeamAccess() {
-        return true;
+    async verifyTeamAccess(teamId) {
+        const teamAccessor = new TeamAccessor();
+        const res = await teamAccessor.getTeamById(teamId);
+        console.log("HERE", res);
+        if (
+            this.leagueId === res?.fantasyleagueID &&
+            this.userId === res?.UserID
+        ) {
+            return true;
+        }
+        return false;
     }
 
     verifyLeagueAccess() {
         return true;
     }
 }
+
+export default AccessVerification;
