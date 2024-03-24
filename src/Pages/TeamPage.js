42fe17055b91542c6154858a57fdee49edc73fc0
Mason <mason.bullock@ucalgary.ca>
2024-03-24 17:45:52 -0600

    team info from db in team header

--- src/Pages/TeamPage.js
+++ src/Pages/TeamPage.js
@@ -4,17 +4,22 @@ import MainHeader from "../Components/MainHeader/MainHeader.js";
 import RosterSection from "../Components/RosterSection/RosterSection.js";
 import Footer from "../Components/Footer/Footer.js";
 import MatchupSnapshot from "../Components/MatchupSnapshot/MatchupSnapshot.js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getCurrentUser } from "aws-amplify/auth";
+import { getCurrentUser, fetchUserAttributes } from "aws-amplify/auth";
 import AccessVerification from "../Helpers/AccessVerification.js";
 import { useNavigate } from "react-router-dom";
+import TeamAccessor from "../Accessors/TeamAccessor.js";
+import { toBeEnabled } from "@testing-library/jest-dom/matchers.js";
 
 const TeamPage = () => {
     const { leagueName, teamName } = useParams();
     const nav = useNavigate();
+    const [user, setUser] = useState({});
+    const [team, setTeam] = useState({ Name: "" });
 
     const verifyAccess = async () => {
+        setUser(await fetchUserAttributes());
         const userId = (await getCurrentUser()).userId;
         const verifier = new AccessVerification(userId, leagueName);
         if (!(await verifier.verifyTeamAccess(teamName))) {
@@ -26,9 +31,21 @@ const TeamPage = () => {
         }
     };
 
+    const getTeam = async () => {
+        const teamAccessor = new TeamAccessor();
+        const tmp = await teamAccessor.getTeamById(teamName);
+        setTeam(tmp);
+    };
+
     useEffect(() => {
         verifyAccess();
+        getTeam();
     }, []);
+
+    useEffect(() => {
+        console.log(user);
+        console.log("REAK TEANM", team);
+    }, [user]);
 
     return (
         <div className="league-page">
@@ -37,15 +54,20 @@ const TeamPage = () => {
                 <div className="team-info-block">
                     <img src={logo}></img>
                     <div className="team-info-text">
-                        <h1>Team</h1>
+                        <h1>
+                            {team.Name.length > 10
+                                ? team.Name.substring(0, 10) + "..."
+                                : team.Name}
+                        </h1>
                         <div className="team-record-info">
                             <h2>2nd Place</h2>
                             <h3>
-                                Record: <span>12</span> - <span>3</span> -
-                                <span> 1</span>
+                                Record: <span>{team.Wins}</span> -{" "}
+                                <span>{team.Losses}</span> -
+                                <span> {team.Draws}</span>
                             </h3>
                         </div>
-                        <p>Owner: Username</p>
+                        <p>Owner: {user.preferred_username}</p>
                     </div>
                 </div>
                 <MatchupSnapshot />
