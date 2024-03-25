3caae586dda8c3de3b041b5906ea961c0fd4791e
Mason <mason.bullock@ucalgary.ca>
2024-03-25 16:32:18 -0600

    bother

--- src/Pages/LandingHomePage.js
+++ src/Pages/LandingHomePage.js
@@ -1,26 +1,32 @@
-import '../Styles/landingHomePage.css';
-import LoginButton from '../Components/LoginButton/LoginButton';
-import Navbar from '../Components/Navbar/Navbar';
+import "../Styles/landingHomePage.css";
+import LoginButton from "../Components/LoginButton/LoginButton.js";
+import Navbar from "../Components/Navbar/Navbar.js";
 
 const LandingPage = () => {
-
-
-  return (
-    <div className='landing-page-overflow-fix'>
-      <Navbar />
-      <div className='page'>
-        <div className='Text'>
-          <div className='Title'>
-            <h1>Welcome to Fantasy Fusion</h1>
-            <h2>The perfect mix of all your favorite fantasy sports in one place.</h2>
-          </div>
-          <div>
-            <LoginButton id='getstartedbutton' buttonStyle='btn--outline'>Get Started</LoginButton>
-          </div>
+    return (
+        <div className="landing-page-overflow-fix">
+            <Navbar />
+            <div className="page">
+                <div className="Text">
+                    <div className="Title">
+                        <h1>Welcome to Fantasy Fusion</h1>
+                        <h2>
+                            The perfect mix of all your favorite fantasy sports
+                            in one place.
+                        </h2>
+                    </div>
+                    <div>
+                        <LoginButton
+                            id="getstartedbutton"
+                            buttonStyle="btn--outline"
+                        >
+                            Get Started
+                        </LoginButton>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </ div>
-  );
+    );
 };
 
 export default LandingPage;
