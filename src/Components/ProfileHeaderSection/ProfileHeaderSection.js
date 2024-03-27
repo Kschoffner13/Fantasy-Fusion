979978350e5c7350b7ab6cc2f0a96183414b1695
Mason <mason.bullock@ucalgary.ca>
2024-03-27 14:26:00 -0600

    Sign out implemented

--- src/Components/ProfileHeaderSection/ProfileHeaderSection.js
+++ src/Components/ProfileHeaderSection/ProfileHeaderSection.js
@@ -2,6 +2,7 @@ import "./style.css";
 import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { fetchUserAttributes } from "@aws-amplify/auth";
+import { signOut } from "aws-amplify/auth";
 
 const ProfileHeaderSection = () => {
     const [username, setUsername] = useState("NAME");
@@ -20,12 +21,24 @@ const ProfileHeaderSection = () => {
         getUserName();
     }, []);
 
+    const signout = async () => {
+        try {
+            await signOut();
+        } catch (error) {
+            console.log(
+                "Failed to log out - You are stuck here forever \nThere are worst places to be"
+            );
+        }
+    };
+
     return (
         <div className="profile-icon">
             <img src="https://picsum.photos/seed/picsum/75/75" />
             <div className="profile-icon-info">
                 <p>{username}</p>
-                <NavLink to="/account">Account</NavLink>
+                <NavLink to="/" onClick={signout}>
+                    Sign Out
+                </NavLink>
             </div>
         </div>
     );
